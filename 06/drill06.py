1c196e179bc6c3fdcb230afdb27161bb3f78558a
JaeHong Park @ Steelval-Port <48243825+Windintosh@users.noreply.github.com>
2021-09-16 15:57:50 +0900

    added circle

--- 06/drill06.py
+++ 06/drill06.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import math
 
 open_canvas()
 
@@ -8,6 +9,12 @@ character = load_image('character.png')
 x = 0
 y = 90
 sq = 1
+sin = 0
+cos = 180
+xmov = 0
+ymov = 0
+xbuf = 0
+ybuf = 0
 while(1):
     if(sq == 1):
         while x < 800:
@@ -43,7 +50,33 @@ while(1):
             character.draw_now(x, y)
             y -= 2
             delay(0.01)
-        sq = 1
+        sq = 5
         continue
-
+    elif(sq == 5):
+        while x < 400:
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character.draw_now(x, y)
+            x += 2
+            delay(0.01)
+        sq = 6
+        continue
+    elif(sq == 6):
+        xbuf = x
+        ybuf = y
+        while sin < 360:
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character.draw_now(x, y)
+            xmov = 200 * math.sin(sin / 360 * 2 * math.pi)
+            ymov = 200 * math.cos(cos / 360 * 2 * math.pi)
+            x = xbuf + xmov
+            y = ybuf + ymov + 200
+            sin += 2
+            cos += 2
+            delay(0.01)
+        sq = 1
+        sin = 0
+        cos = 180
+        continue
 close_canvas()
